69ccd0de97514d1d7f374ca36ac1507dff4d95f8
matttei <matt.tei@digital.justice.gov.uk>
2020-10-20 15:28:19 +0100

    Intial commit.
    Co-authored-by: Tomas Destefi <tomas.destefi@digital.justice.gov.uk>
    Co-authored-by: Natalie Seeto <natalie.seeto@digital.justice.gov.uk>
    Co-authored-by: Brendan Butler <brendan.butler@digital.justice.gov.uk>

---
+++ lib/fb/jwt/auth.rb
@@ -0,0 +1,10 @@
+require "fb/jwt/auth/version"
+
+module Fb
+  module Jwt
+    module Auth
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/fb/jwt/auth/version.rb
@@ -0,0 +1,7 @@
+module Fb
+  module Jwt
+    module Auth
+      VERSION = "0.1.0"
+    end
+  end
+end
